df8c931999b25c1fb14fde2daf1cac98fa1d183d
Matthew Smilansky <msmilansky@gmail.com>
2024-07-31 20:00:17 -0700

    feat: LoadConfig()

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Timer struct {
 	Name       string `json:name`
 	Countdown  int    `json:countdown`
@@ -8,5 +15,28 @@ type Timer struct {
 }
 
 type Config struct {
-	Timers Timer[] `json:timers`
+	Timers []Timer `json:timers`
+}
+
+func LoadConfig(file string) (*Config, error) {
+	// open config file
+	configFile, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("Could not open config file: %w", err)
+	}
+	defer configFile.Close()
+
+	// read config file
+	byteValue, err := ioutil.ReadAll(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("Could not read config file: %w", err)
+	}
+
+	// parse the read config file and store it in 'config'
+	var config Config
+	if err := json.Unmarshal(byteValue, &config); err != nil {
+		return nil, fmt.Errorf("Could not unmarshal config: %w", err)
+	}
+
+	return &config, nil
 }
